f704afbf6a6957009e4197c20a0cb87f37aa6ebc
IaraFV <103749046+IaraFV@users.noreply.github.com>
2022-07-01 16:31:13 -0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { PessoasComponent } from './views/pessoas/pessoas.component';
+import { EquipesComponent } from './views/equipes/equipes.component';
+import { TaskComponent } from './views/task/task.component';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path:'task', component: TaskComponent},
+  {path:'equipes', component: EquipesComponent},
+  {path:'pessoas', component: PessoasComponent},
+  {path:'', component: DashboardComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'Pessoas', cols: 1, rows: 1 },
+          { title: 'projetos', cols: 1, rows: 1 },
+          { title: 'Card 3', cols: 1, rows: 1 },
+          { title: 'Card 4', cols: 1, rows: 1 }
+        ];
+      }
+
+      return [
+        { title: 'Pessoas', cols: 2, rows: 1 },
+        { title: 'projetos', cols: 1, rows: 1 },
+        { title: 'Card 3', cols: 1, rows: 2 },
+        { title: 'Card 4', cols: 1, rows: 1 }
+      ];
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+chartType = 'line';
+
+  chartDatasets = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'My First dataset' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'My Second dataset' }
+  ];
+
+  chartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+  chartColors = [
+    {
+      backgroundColor: 'rgba(105, 0, 132, .2)',
+      borderColor: 'rgba(200, 99, 132, .7)',
+      borderWidth: 2,
+    },
+    {
+      backgroundColor: 'rgba(0, 137, 132, .2)',
+      borderColor: 'rgba(0, 10, 130, .7)',
+      borderWidth: 2,
+    }
+  ];
+
+  chartOptions: any = {
+    responsive: true
+  };
+
+  chartClicked(event: any) {
+    console.log(event);
+  }
+
+  chartHovered(event: any) {
+    console.log(event);
+  }
+
+  panelOpenState = false;
+
+}
